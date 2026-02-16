84307ad4086c4c2293a05e30b6b5b52d7d0146c9
mehrdad <mehrdadasg@gmail.com>
2026-02-16 23:26:02 +0330

    add fav

--- src/service/getPregnancyFeature.ts
+++ src/service/getPregnancyFeature.ts
@@ -1,10 +1,8 @@
-export function getPregnancyFeature(locale: string = "en") {
-  return fetch(`${process.env.BASE_URL}/home/features/pregnancy`, { headers: { lang: locale } })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(`API Error [${response.status}] ${response.url}`);
-        //throw new Error("Network response was not ok");
-      }
-      return response.json();
-    });
+export async function getPregnancyFeature(locale: string = "en") {
+  const response = await fetch(`${process.env.BASE_URL}/home/features/pregnancy`, { headers: { lang: locale } });
+  if (!response.ok) {
+    const body = await response.text();
+    throw new Error(`API Error [${response.status}] ${response.url} | ${body}`);
+  }
+  return response.json();
 }
